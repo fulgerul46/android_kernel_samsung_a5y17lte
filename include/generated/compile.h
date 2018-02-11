577964c937c5e30c412752d11c42091b9fbd31ef
fulgerul46 <fulgerul46@yahoo.com>
2018-02-11 17:09:10 +0200

    Upload Samsung stock kernel source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Feb 11 16:10:03 EET 2018"
+#define LINUX_COMPILE_BY "fulgerul"
+#define LINUX_COMPILE_HOST "fulgerul"
+#define LINUX_COMPILER "gcc version 4.9.4 20160401 (optimized) (UBERTC-4.9.4) "
